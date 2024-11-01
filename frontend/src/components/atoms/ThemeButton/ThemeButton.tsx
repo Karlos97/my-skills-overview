7643ec6103aa695e03f7f94aedda85c54d7cb56a
karlos.97 <k.minta97@gmail.com>
2024-11-01 19:30:59 +0100

    Adjust theme icon

--- frontend/src/components/atoms/ThemeButton/ThemeButton.tsx
+++ frontend/src/components/atoms/ThemeButton/ThemeButton.tsx
@@ -9,7 +9,7 @@ const ThemeButton = () => {
   return (
     <Button
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-      className="mt-4 px-4 py-2 bg-gray-50 dark:bg-gray-800 hover:bg-blue-500 hover:dark:bg-gray-600 text-gray-700 hover:text-white dark:text-white rounded-md cursor-pointer"
+      className="mt-4 px-4 py-2 bg-gray-50 dark:bg-gray-800 hover:bg-blue-500 hover:dark:bg-gray-600 !text-gray-700 hover:!text-white dark:!text-white rounded-md cursor-pointer"
     >
       {theme === 'dark' ? <SunIcon /> : <MoonIcon />}
     </Button>
